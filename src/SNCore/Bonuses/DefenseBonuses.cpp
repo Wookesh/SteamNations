8f12f1077bce95ddc96f8ddc4a010b8761ba9673
Kasjan Siwek <kasjan.siwek@gmail.com>
2015-06-11 12:57:39 +0200

    Re-added bonuses

--- src/SNCore/Bonuses/DefenseBonuses.cpp
+++ src/SNCore/Bonuses/DefenseBonuses.cpp
@@ -12,12 +12,12 @@ DefenseT1Bonus::DefenseT1Bonus() : Bonus (1, BonusType::Def, SNCfg::DEF_TECH_DES
 }
 
 bool DefenseT1Bonus::apply(Player *player) {
-/*	if (!canApply(player))
+	if (!canApply(player))
 		return false;
 	
 	for (Prototype *element : player->prototypes()) {
 		element->addHealthBonus(0.1f);
-	}*/
+	}
 	
 	return true;
 }
